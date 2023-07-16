216ddc49d17fd9ba51c6db9a00c6ebedb139c87a
Corentin Ferrier <ferrier.corentin@wanadoo.fr>
2023-07-16 22:27:48 +0200

    Init

---
+++ src/main/kotlin/corentin/ferrier/picturemap/PictureMapApplication.kt
@@ -0,0 +1,11 @@
+package corentin.ferrier.picturemap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PictureMapApplication
+
+fun main(args: Array<String>) {
+	runApplication<PictureMapApplication>(*args)
+}
---
+++ src/test/kotlin/corentin/ferrier/picturemap/PictureMapApplicationTests.kt
@@ -0,0 +1,13 @@
+package corentin.ferrier.picturemap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PictureMapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
